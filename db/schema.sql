638a6885a2ef91596d8b0378660fdab9fe20831e
Marc Maldonado <marc@telecomelite.com>
2020-03-13 12:56:12 -0400

    got it connected to db and server up

---
+++ db/schema.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Cheeseburger');
+INSERT INTO burgers (name) VALUES ('Chiliburger');
+INSERT INTO burgers (name) VALUES ('Steakburger');
+INSERT INTO burgers (name) VALUES ('Veggie Burger');
+INSERT INTO burgers (name) VALUES ('Salmonburger');
+INSERT INTO burgers (name) VALUES ('Turkeyburger');
