dd4ef5476859b9c69aee8f01289d6d156545f813
Marc Maldonado <marc@telecomelite.com>
2020-03-14 14:07:18 -0400

    fixed connection for heroku deploy

--- config/connection.js
+++ config/connection.js
@@ -16,13 +16,7 @@ connection= mysql.createConnection({
 };
 
 // Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+connection.connect() 
 
 // Export connection for our ORM to use.
 module.exports = connection;
